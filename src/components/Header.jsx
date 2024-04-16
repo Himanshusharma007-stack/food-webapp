6e6a52d6f5b4d7e2a092bcb2eaaf4eb6c23b9369
Personal <Personal@Himanshus-MacBook-Pro.local>
2024-04-17 00:38:47 +0530

    show-categories-on-homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,25 +3,25 @@ export default function Header() {
     <>
       <div className="grid grid-cols-3 h-16 items-center font-medium px-4">
         <div>
-          <button className="flex items-center">
+          <button className="flex items-center hover:scale-110 duration-300">
             <img
               src="../src/assets/header/bw-food-icon.png"
               className="h-10 w-10 transition-all"
             />
-            <span className="transition-all hover:font-semibold">
+            <span className="transition-all">
               PlatterPulse
             </span>
           </button>
         </div>
 
         <ul className="flex gap-8 justify-center">
-          <li className="hover:font-semibold">
+          <li className="hover:scale-110 duration-300">
             <button>Home</button>
           </li>
-          <li className="hover:font-semibold">
+          <li className="hover:scale-110 duration-300">
             <button>About</button>
           </li>
-          <li className="hover:font-semibold">
+          <li className="hover:scale-110 duration-300">
             <button>Contact us</button>
           </li>
         </ul>
@@ -30,7 +30,7 @@ export default function Header() {
           <button>
             <img
               src="../src/assets/header/cart.png"
-              className="h-10 w-10 hover:h-11 hover:w-11"
+              className="h-10 w-10 hover:scale-110 duration-300"
             />
           </button>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,45 @@
+import categories from "../../db/categories.json";
+
 export default function Home() {
   return (
     <>
       <div class="relative">
         <img
           src="../src/assets/home/Carousel.jpeg"
-          class="h-screen w-screen"
+          class="h-96 w-screen object-cover"
           alt="Carousel Image"
         />
         <div class="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center opacity-0 transition-opacity duration-1000 animate-fade-in">
-          <h1 class="text-4xl font-bold mb-2">Elevate Your Dining Experience</h1>
+          <h1 class="text-4xl font-bold mb-2">
+            Elevate Your Dining Experience
+          </h1>
           <p class="text-lg">Without Stepping Out</p>
+        </div>
+      </div>
+
+      <div className="my-8">
+        <h1 className="text-center font-bold text-3xl">Select a category</h1>
+        <div className="flex flex-wrap justify-center mb-10 mt-4">
+          {categories.map((category) => (
+            <div className="h-60 w-64 rounded-lg mt-5 mx-4 hover:scale-105 duration-300">
+              <div className="rounded-lg">
+                <img
+                  src={category.imageUrl}
+                  alt={category.name}
+                  className="object-cover h-44 w-64 rounded-lg"
+                />
+              </div>
+
+              <div>
+                <h4 className="text-center text-xl font-semibold">
+                  {category.name}
+                </h4>
+                <p className="text-center truncate mx-2">
+                  {category.description}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
